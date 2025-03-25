91b567e0e949a1e2f00faedaf0c34feac71ff894
Cruz Sharaid <cruzsharaid@gmail.com>
2025-03-24 23:19:00 -0400

    Delete n_queens directory

--- n_queens/reynas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReynasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reynas'
--- n_queens/reynas/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from.views import resolver_nqueens
-
-
-
-urlpatterns = [
-    path('resolver/<int:n>/',resolver_nqueens,name='resolver_nqueens'),
-
-]
